bfb11461faf797a2222d30c1d3f84f47f7c7dfb2
Liliya Pustovoyt <lila@pustovoyt.me>
2020-05-20 13:41:24 -0400

    Initial Commit

---
+++ iOSTodoSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSTodoSample
+//
+//  Created by Lila Pustovoyt on 5/20/20.
+//  Copyright © 2020 Lila Pustovoyt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
